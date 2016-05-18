1e2d3313fb78b6927a36f97578d4e644dfb860d1
g-drol <guyd1491@hotmail.fr>
2016-05-18 14:41:35 -0400

    Moving cat, Movable Box

---
+++ Assets/Scripts/MovableBoxController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovableBoxController : MonoBehaviour {
+
+	//private Transform _player;
+
+	// Use this for initialization
+	void Start () {
+		//_player = GameObject.FindGameObjectsWithTag ("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	//Working in sprites
+	private Rigidbody2D _characterController;
+
+	// Use this for initialization
+	void Start () {
+		
+		_characterController = GetComponent<Rigidbody2D>();
+	}
+
+	void Update ()
+	{
+		float horizontal = Input.GetAxis ("Horizontal");
+		_characterController.velocity = new Vector2 (horizontal*10, _characterController.velocity.y);
+	}
+}
