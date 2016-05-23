074c96d216299e1deb8ca924ba3001c139a6e24f
g-drol <guyd1491@hotmail.fr>
2016-05-23 11:14:58 -0400

    Still on Push and Pull

--- Assets/Scripts/PushPullController.cs
+++ Assets/Scripts/PushPullController.cs
@@ -3,13 +3,12 @@ using System.Collections;
 
 public class PushPullController : MonoBehaviour {
 
-	//private Joint2D jointBoxPlayer;
-
-	private Rigidbody2D _movableBox;
+	private Joint2D jointBoxPlayer;
+	private GameObject _movableBox;
 
 	// Use this for initialization
 	void Start () {
-		_movableBox = GetComponent<Rigidbody2D> ();
+		_movableBox = GetComponent<GameObject> ();
 	}
 
 	// Update is called once per frame
@@ -17,33 +16,28 @@ public class PushPullController : MonoBehaviour {
 
 	}
 
-	private void OnControllerColliderHit(ControllerColliderHit hit){
+	private void OnCollisionEnter2D(Collision2D col) {
 
-		if (Input.GetKey (KeyCode.H) && hit.gameObject.tag == "Player") {
-			_movableBox.velocity = hit.gameObject.GetComponent<Rigidbody2D> ().velocity;
-		}
-	}
-
-	/*private void OnCollisionEnter(Collision col) {
+		Debug.Log ("Enters collision");
 
 		if(col.gameObject.tag == "Player"){
-			gameObject.AddComponent<FixedJoint2D>();
-		}
-
-		jointBoxPlayer = GetComponent<FixedJoint2D>() ;
-	}
-
-	private void OnCollisionStay () {
-
-		GameObject player = GameObject.FindWithTag("Player");
-
-		if (Input.GetKey(KeyCode.H)) {
-			jointBoxPlayer.connectedBody = player.gameObject.GetComponent<Rigidbody2D>();
+			Debug.Log ("Enters collision");
+			_movableBox.AddComponent<FixedJoint2D>();
+			jointBoxPlayer = GetComponent<FixedJoint2D>() ;
 		}
 	}
 
-	private void OnCollisionExit(Collision col) {
+	private void OnCollisionStay2D(Collision2D col) {
+
+		if(col.gameObject.tag == "Player" && Input.GetKey(KeyCode.H)){
+			Debug.Log ("Stays collision");
+			jointBoxPlayer.connectedBody = col.gameObject.GetComponent<Rigidbody2D>();
+		}
+	}
+
+	private void OnCollisionExit2D(Collision2D col) {
+		Debug.Log ("Leaves collision");
 		Destroy (jointBoxPlayer);
-	}*/
+	}
 
 }
