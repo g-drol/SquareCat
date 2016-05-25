34992a28366979696e0daac56044589556933617
g-drol <guyd1491@hotmail.fr>
2016-05-25 13:07:15 -0400

    Working on gravity

---
+++ Assets/Scripts/GChangeAuthorizer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class GChangeAuthorizer : MonoBehaviour {
+
+	/*//List of rigidbodies
+	private Rigidbody2D[] _movableAndPlayer;
+
+	// Use this for initialization
+	void Start () {
+		_movableAndPlayer = GetComponent<Rigidbody2D> ();
+	}
+
+	void MakeKinematic(bool isKinematicVar){
+		for (int i = 0; i < _movableAndPlayer.Length; i++) {
+			_movableAndPlayer [i].isKinematic = isKinematicVar;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+	}*/
+}
---
+++ Assets/Scripts/GChangeController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct GravityChange {
+	public DeviceOrientation deviceOrientation;
+}
+
+public class GChangeController : MonoBehaviour {
+	
+	/*//Delegate and event
+	public delegate void GravityChangeDelegate(GravityChange gravity);
+	public static event GravityChangeDelegate onGravityChange = delegate {};
+
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Application.isEditor) {
+			if(Input.GetKey(KeyCode.I))
+				Physics2D.gravity = new Vector2 (0, -1f);
+			if(Input.GetKey(KeyCode.J))
+				Physics2D.gravity = new Vector2 (-1f, 0);
+			if(Input.GetKey(KeyCode.K))
+				Physics2D.gravity = new Vector2 (0, 1f);
+			if(Input.GetKey(KeyCode.M))
+				Physics2D.gravity = new Vector2 (1f, 0);
+		}
+
+		switch (Input.deviceOrientation) {
+		case DeviceOrientation.LandscapeLeft:
+			Physics2D.gravity = new Vector2 (0, -1f);
+			break;
+		case DeviceOrientation.PortraitUpsideDown:
+			Physics2D.gravity = new Vector2 (-1f, 0);
+			break;
+		case DeviceOrientation.LandscapeRight:
+			Physics2D.gravity = new Vector2 (0, 1f);
+			break;
+		case DeviceOrientation.Portrait:
+			Physics2D.gravity = new Vector2 (1f, 0);
+			break;
+		}
+	}*/
+}
