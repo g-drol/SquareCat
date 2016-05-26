5f2bb26760b697c002e058a1b2fb996e8beee9e5
g-drol <guyd1491@hotmail.fr>
2016-05-26 14:33:50 -0400

    Gravity

--- Assets/Scripts/GChangeController.cs
+++ Assets/Scripts/GChangeController.cs
@@ -3,44 +3,139 @@ using System.Collections;
 
 public struct GravityChange {
 	public DeviceOrientation deviceOrientation;
+	public Vector2 nGravity;
 }
 
 public class GChangeController : MonoBehaviour {
-	
-	/*//Delegate and event
+
+	//Delegate and event
 	public delegate void GravityChangeDelegate(GravityChange gravity);
 	public static event GravityChangeDelegate onGravityChange = delegate {};
+	//List of movables
+	private Rigidbody2D[] _movable;
+	//players
+	private Rigidbody2D _player;
 
 	void Start () {
+
+		_movable = null;
+		GameObject[] temp = GameObject.FindGameObjectsWithTag ("Movable");
+		//Getting all the rigidbodies: movable and player
+		for (int i = 0; i < temp.Length; i++){
+			_movable[i] = temp[i].GetComponent<Rigidbody2D>();
+		}
+		_player = GameObject.FindGameObjectWithTag ("Player").GetComponent<Rigidbody2D>();
 	}
-	
-	// Update is called once per frame
+
+	/// <summary>
+	/// Checking if all the rigidbodies are colliding with something
+	/// Prevents gravity switch in the middle of an action
+	/// Make sure gravity effect is big enough to make the switch as quick as possible
+	/// </summary>
+	/// <returns><c>true</c>, if they're all in place, <c>false</c> otherwise.</returns>
+	/// <param name="listToCheck">List to check.</param>
+	bool Colliding(Rigidbody2D[] listToCheck){
+
+		Debug.Log ("In first colliding method");
+		//Raycast in the direction of the gravity, get the collider the rest hits and then IsTouching
+		RaycastHit2D rayHit;
+
+		//For loop on all the rigidBodies
+		for (int i = 0; i < listToCheck.Length; i++) {
+
+			//Raycast in the direction of the gravity to see if they are touching the element closest to them
+			//If a box is over a box, the box under it will return false for as long as it doesn't touch a wall
+			rayHit = Physics2D.Raycast (listToCheck [i].position, Physics2D.gravity, 1f);
+			if (rayHit.collider == null || !rayHit.collider.IsTouching (listToCheck [i].GetComponent<Collider2D> ())) {
+				return false;
+				//RayCast collider in Unity changed to ignore themselves
+				//Edit --> Project Settings --> Physics 2D --> Queries start in Collider (Unchecked)
+			}
+		}
+		Debug.Log ("Do I get here?");
+		return true;
+	}
+
+	/// <summary>
+	/// Ovearloading
+	/// Checking if all the rigidbodies are colliding with something
+	/// Prevents gravity switch in the middle of an action
+	/// Make sure gravity effect is big enough to make the switch as quick as possible
+	/// </summary>
+	/// <returns><c>true</c>, if they're all in place, <c>false</c> otherwise.</returns>
+	/// <param name="listToCheck">List to check.</param>
+	bool Colliding(Rigidbody2D elementToCheck){
+
+		//Raycast in the direction of the gravity, get the collider the rest hits and then IsTouching
+		RaycastHit2D rayHit;
+		//Raycast in the direction of the gravity to see if they are touching the element closest to them
+		//If a box is over a box, the box under it will return false for as long as it doesn't touch a wall
+		rayHit = Physics2D.Raycast (elementToCheck.position, Physics2D.gravity, 1f);
+		if (rayHit.collider == null || !rayHit.collider.IsTouching (elementToCheck.GetComponent<Collider2D> ())) {
+			return false;
+			//RayCast collider in Unity changed to ignore themselves
+			//Edit --> Project Settings --> Physics 2D --> Queries start in Collider (Unchecked)
+		}
+		return true;
+	}
+
 	void Update () {
 
-		if (Application.isEditor) {
-			if(Input.GetKey(KeyCode.I))
-				Physics2D.gravity = new Vector2 (0, -1f);
-			if(Input.GetKey(KeyCode.J))
-				Physics2D.gravity = new Vector2 (-1f, 0);
-			if(Input.GetKey(KeyCode.K))
-				Physics2D.gravity = new Vector2 (0, 1f);
-			if(Input.GetKey(KeyCode.M))
-				Physics2D.gravity = new Vector2 (1f, 0);
-		}
+		//Creating a GravityChange for events
+		GravityChange gravityChange = new GravityChange ();
 
-		switch (Input.deviceOrientation) {
-		case DeviceOrientation.LandscapeLeft:
-			Physics2D.gravity = new Vector2 (0, -1f);
-			break;
-		case DeviceOrientation.PortraitUpsideDown:
-			Physics2D.gravity = new Vector2 (-1f, 0);
-			break;
-		case DeviceOrientation.LandscapeRight:
-			Physics2D.gravity = new Vector2 (0, 1f);
-			break;
-		case DeviceOrientation.Portrait:
-			Physics2D.gravity = new Vector2 (1f, 0);
-			break;
-		}
-	}*/
-}
+		//Check if we aren't already in the given orientation
+		//if (!Input.deviceOrientation.) {
+		//If All the movable bodies are already colliding with something
+		if(Colliding(_movable) && Colliding(_player)){
+				//Editor and testing purposes
+				if (Application.isEditor) {
+					if(Input.GetKey(KeyCode.I)){
+						gravityChange.nGravity = new Vector2 (0, -1f);
+						gravityChange.deviceOrientation = DeviceOrientation.LandscapeLeft;
+						onGravityChange (gravityChange);
+					}
+					if(Input.GetKey(KeyCode.J)){
+						gravityChange.nGravity = new Vector2 (-1f, 0);
+						gravityChange.deviceOrientation = DeviceOrientation.PortraitUpsideDown;
+						onGravityChange (gravityChange);
+					}
+					if(Input.GetKey(KeyCode.K)){
+						gravityChange.nGravity = new Vector2 (0, 1f);
+						gravityChange.deviceOrientation = DeviceOrientation.LandscapeRight;
+						onGravityChange (gravityChange);
+					}
+					if(Input.GetKey(KeyCode.M)){
+						gravityChange.nGravity = new Vector2 (1f, 0);
+						gravityChange.deviceOrientation = DeviceOrientation.Portrait;
+						onGravityChange (gravityChange);
+					}
+				}
+
+				//Mobile Phone settings
+				switch (Input.deviceOrientation) {
+				case DeviceOrientation.LandscapeLeft:
+					gravityChange.nGravity = new Vector2 (0, -1f);
+					gravityChange.deviceOrientation = DeviceOrientation.LandscapeLeft;
+					onGravityChange (gravityChange);
+					break;
+				case DeviceOrientation.PortraitUpsideDown:
+					gravityChange.nGravity = new Vector2 (-1f, 0);
+					gravityChange.deviceOrientation = DeviceOrientation.PortraitUpsideDown;
+					onGravityChange (gravityChange);
+					break;
+				case DeviceOrientation.LandscapeRight:
+					gravityChange.nGravity = new Vector2 (0, 1f);
+					gravityChange.deviceOrientation = DeviceOrientation.LandscapeRight;
+					onGravityChange (gravityChange);
+					break;
+				case DeviceOrientation.Portrait:
+					gravityChange.nGravity = new Vector2 (1f, 0);
+					gravityChange.deviceOrientation = DeviceOrientation.Portrait;
+					onGravityChange (gravityChange);
+					break;
+				}//end switch
+			}//end if colliding
+		//}//end if device orientation is different
+	}//end Update
+}//end class
---
+++ Assets/Scripts/GravityController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityController : MonoBehaviour {
+
+	//List of rigidbodies
+	private GameObject[] _movable;
+
+	void Start () {
+	
+		//Getting all the rigidbodies: movable and player
+		_movable = GameObject.FindGameObjectsWithTag("Movable");
+		//Adding new method to event
+		GChangeController.onGravityChange += OnGravityChange;
+	}
+
+	/// <summary>
+	/// Call when object is destroyed
+	/// - Remove OnGravityChange from the method list of GChangeController
+	/// </summary>
+	void OnDestroy(){
+		GChangeController.onGravityChange -= OnGravityChange;	
+	}
+
+	/// <summary>
+	/// Makes gameObjects with the Movable tag Kinematic or not depending on the bool given.
+	/// </summary>
+	/// <param name="isKinematicVar">If set to <c>true</c>, every "Movable" becomes kinematic, <c>false</c> otherwise.</param>
+	/// <param name="listToChange">List of RigidBodies</param>
+	void MakeKinematic(bool isKinematicVar, GameObject[] listToModify){
+
+		Debug.Log ("in kinematic method");
+		for (int i = 0; i < listToModify.Length; i++) {
+			Debug.Log ("in for loop");
+			//Ignoring the player because the player is always non-kinematic
+			if (listToModify [i].gameObject.tag == "Movable") {
+				listToModify [i].GetComponent<Rigidbody2D>().isKinematic = isKinematicVar;
+				Debug.Log ("Changed kinematic");
+			}
+		}
+
+	}
+
+	void Update () {
+	
+	}
+
+	void OnGravityChange(GravityChange gravityChange){
+
+		Debug.Log (_movable.Length);
+		Physics2D.gravity = gravityChange.nGravity;
+	}
+}
