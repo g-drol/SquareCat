8b3c7b350d986507da5425d62f3e49746d24e7f5
g-drol <guyd1491@hotmail.fr>
2016-06-05 19:54:56 -0400

    Main Menu script

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public string firstScene;
+
+	public void StartGame()
+	{
+			SceneManager.LoadScene(firstScene, LoadSceneMode.Single);
+	}
+}
