32a66eab73aae4fb50eeccbb05e2a8b9fca7452f
g-drol <guyd1491@hotmail.fr>
2016-06-06 13:39:54 -0400

    Started Switches

---
+++ Assets/Scripts/SwitchController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum SwitchAction{
+	OpenDoor,
+	UnlockBlock,
+	UnlockSwitch
+}
+
+public class SwitchController : MonoBehaviour {
+
+	public bool isSwitchTimed;
+	public SwitchAction switchAction;
+
+	private GameObject _door;
+
+	// Use this for initialization
+	void Start () {
+		_door = GameObject.FindGameObjectWithTag ("Door");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnTriggerEnter(Collider other){
+		Debug.Log ("trigger enter");
+		switch (switchAction) {
+		case SwitchAction.OpenDoor:
+			_door.GetComponent<Animator> ().SetBool ("isDoorOpen", true);
+			break;
+		case SwitchAction.UnlockBlock:
+			break;
+		case SwitchAction.UnlockSwitch:
+			break;
+		}
+	
+	}
+}
