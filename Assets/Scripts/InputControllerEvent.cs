bd536108b2d8aa151a96a3f4acfbdc551d86d472
g-drol <guyd1491@hotmail.fr>
2016-10-04 20:30:25 -0400

    Input Controller Modification
    - Changed speed threshold for cat animation
    - Changed the presentation of the scene
    - Input Controller now handles mouse
    - Input Controller handles Touches
    - Input Controller should be able to make the difference between hold
    and tap (Still working on it)

--- Assets/Scripts/InputControllerEvent.cs
+++ Assets/Scripts/InputControllerEvent.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public enum TouchInputType {
 	Tap,
-	Hold
+	Hold,
+	Swipe
 }
 	
 public struct TouchInput {
 	public Vector2 position;
 	public TouchInputType inType;
+	public Vector2 swipeDistance;
 }
 	
 public class InputControllerEvent : MonoBehaviour {
@@ -20,53 +22,89 @@ public class InputControllerEvent : MonoBehaviour {
 	//Constants
 	private const int kMaxNumTouches = 2;
 	private const float kMaxDistanceTap = 1f;
+	private const float kTimeforHold = 0.1f; //arbitrary
+
+	private bool _isHolding = false;
 
 	//Vector array of start positions
-	private Vector2 [] _startPosition = new Vector2[kMaxNumTouches];
-	private bool[] _isTap = new bool[kMaxNumTouches];
+	public Vector2 _startPosition = new Vector2();
+
+	//private Vector2 _startPosition = new Vector2[kMaxNumTouches];
+	//private bool[] _isTap = new bool[kMaxNumTouches];
 
 	void Update(){
 
-		Touch [] touchList;
+		//Touch [] touchList;
+		Touch touch;
 		TouchInput touchInput = new TouchInput ();
+
+		mousePosition (touchInput);
 
 		if (Input.touchCount != 0) {
 
-			touchList = Input.touches;
+			_isHolding = false;
 
-			for (int i = 0; i < touchList.Length && i < kMaxNumTouches; i++) {
+			for (int i = 0; i < Input.touchCount; i++) {
 
-				Vector2 touchPosition = Camera.main.ScreenToWorldPoint (touchList[i].position);
+				touch = Input.GetTouch(i);
+				Vector2 touchPosition = Camera.main.ScreenToWorldPoint (touch.position);
 
-				switch (touchList [i].phase) {
+				switch (touch.phase) {
 
 				case TouchPhase.Began:
-					_startPosition [i] = touchPosition;
-					_isTap [i] = true;
+					_startPosition = touchPosition;
 					break;
 
 				case TouchPhase.Moved:
+					touchInput.swipeDistance = touch.deltaPosition;
+					touchInput.inType = TouchInputType.Swipe;
+					touchInput.position = touchPosition;
+					onTouchInput (touchInput);
+					break;
+
 				case TouchPhase.Stationary:
-					if (Vector2.Distance (_startPosition[i], touchPosition) > kMaxDistanceTap) {
-						_isTap [i] = false;					
-					}
+					_isHolding = true;
+					//If Ended gets ignored
 					touchInput.inType = TouchInputType.Hold;
 					touchInput.position = touchPosition;
 					onTouchInput (touchInput);
 					break;
 
 				case TouchPhase.Ended:
-					if (_isTap [i]) {
+					if (touch.deltaTime > kTimeforHold || _isHolding)
+						touchInput.inType = TouchInputType.Hold;
+					else
 						touchInput.inType = TouchInputType.Tap;
-						touchInput.position = touchPosition;
-						onTouchInput (touchInput);
-					}
+					touchInput.position = touchPosition;
+					onTouchInput (touchInput);
 					break;
-
 				}
 			}
 		}
+	}
+
+	void mousePosition(TouchInput touchInput){
+
+		if (Input.GetMouseButtonDown (0)) {
+			_startPosition = (Vector2)Input.mousePosition;
+		}
+
+		if (Input.GetMouseButton (0)) {
+			touchInput.inType = TouchInputType.Hold;
+			Debug.Log (touchInput.inType);
+			touchInput.position = (Vector2)Input.mousePosition;
+			Debug.Log (touchInput.position);
+			onTouchInput (touchInput);
+		}
 	
-	
+		if(Input.GetMouseButtonUp(0)){
+			touchInput.swipeDistance = _startPosition - (Vector2)Input.mousePosition;
+			if (touchInput.swipeDistance.Equals (Vector2.zero))
+				touchInput.inType = TouchInputType.Tap;
+			else
+				touchInput.inType = TouchInputType.Swipe;
+			touchInput.position = (Vector2)Input.mousePosition;
+			onTouchInput (touchInput);
+		}
 	}
 }
