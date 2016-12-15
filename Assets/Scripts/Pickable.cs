0667883d1aedfff36c73abc0a692754309f38f2c
g-drol <guyd1491@hotmail.fr>
2016-12-14 21:11:39 -0500

    Pickable completed

---
+++ Assets/Scripts/Pickable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Pickable : MonoBehaviour {
+
+	[SerializeField]
+	private int ID;
+
+	private void OnTriggerEnter2D(Collider2D other){
+		PickableController.instance.PickUp (this);
+		Destroy (gameObject);
+	}
+
+	public int getID(){
+		return ID;
+	}
+}
---
+++ Assets/Scripts/PickableController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickableController : MonoBehaviour {
+
+	[SerializeField]
+	private Image[] imageList;
+
+	private static PickableController pickableC;
+	public static PickableController instance{
+		get { return pickableC; }
+	}
+		
+	void Awake(){
+		pickableC = this;
+	}
+
+	public void PickUp(Pickable pickable){
+		imageList [pickable.getID()].gameObject.SetActive (true);
+	}
+
+}
